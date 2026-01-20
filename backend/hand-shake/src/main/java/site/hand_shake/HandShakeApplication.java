b951f296cde879f9bc7fef648c51c7d91839f9fe
김민수 <visang96@naver.com>
2026-01-20 15:32:16 +0900

    chore: 프론트엔드·백엔드·AI 모노레포 초기 세팅

---
+++ backend/hand-shake/src/main/java/site/hand_shake/HandShakeApplication.java
@@ -0,0 +1,13 @@
+package site.hand_shake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandShakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandShakeApplication.class, args);
+	}
+
+}
---
+++ backend/hand-shake/src/test/java/site/hand_shake/HandShakeApplicationTests.java
@@ -0,0 +1,13 @@
+package site.hand_shake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandShakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
