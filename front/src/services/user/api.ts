65417e6c7a6e3c17b07a7adab5983dbb38d0179b
김동영 <wwweric12@gmail.com>
2026-01-23 13:53:35 +0900

    feat: user 관련 api 및 hooks 구현

---
+++ front/src/services/user/api.ts
@@ -0,0 +1,36 @@
+import { api } from '@/services/api';
+import {
+  CheckNicknameRequest,
+  CheckNicknameResponse,
+  DstiResponse,
+  SubmitDstiRequest,
+  UpdateCareerRequest,
+  UpdateExperienceRequest,
+  UpdateGithubRequest,
+  UpdateNetworksRequest,
+  UpdatePositionsRequest,
+  UpdateSelfIntroRequest,
+  UpdateTechSkillsRequest,
+  UserCardsResponse,
+  UserProfile,
+} from '@/types/user';
+
+export const userApi = {
+  getUserInfo: () => api.get<UserProfile>('/user/info'),
+  registerUserProfile: (data: UserProfile) => api.post('/user/info', data),
+
+  checkNickname: (data: CheckNicknameRequest) =>
+    api.post<CheckNicknameResponse>('/user/nickname', data),
+
+  submitDsti: (data: SubmitDstiRequest) => api.post<DstiResponse>('/user/dsti', data),
+
+  getUserCards: () => api.get<UserCardsResponse>('/user/card'),
+
+  updateExperience: (data: UpdateExperienceRequest) => api.put('/user/experience', data),
+  updateCareer: (data: UpdateCareerRequest) => api.put('/user/career', data),
+  updateGithub: (data: UpdateGithubRequest) => api.put('/user/github', data),
+  updateSelfIntro: (data: UpdateSelfIntroRequest) => api.put('/user/self-intro', data),
+  updateTechSkills: (data: UpdateTechSkillsRequest) => api.put('/user/tech-skills', data),
+  updatePositions: (data: UpdatePositionsRequest) => api.put('/user/position', data),
+  updateNetworks: (data: UpdateNetworksRequest) => api.put('/user/networks', data),
+};
---
+++ front/src/services/user/hooks.ts
@@ -0,0 +1,125 @@
+import { QUERY_KEYS } from '@/constants/queryKeys';
+import { userApi } from '@/services/user/api';
+import {
+  SubmitDstiRequest,
+  UpdateCareerRequest,
+  UpdateExperienceRequest,
+  UpdateGithubRequest,
+  UpdateNetworksRequest,
+  UpdatePositionsRequest,
+  UpdateSelfIntroRequest,
+  UpdateTechSkillsRequest,
+  UserProfile,
+} from '@/types/user';
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+export const useUserProfile = () => {
+  return useQuery({
+    queryKey: QUERY_KEYS.USER.INFO(),
+    queryFn: userApi.getUserInfo,
+  });
+};
+
+export const useUserCards = () => {
+  return useQuery({
+    queryKey: QUERY_KEYS.USER.CARD(),
+    queryFn: userApi.getUserCards,
+  });
+};
+
+export const useCheckNicknameMutation = () => {
+  return useMutation({
+    mutationFn: (nickname: string) => userApi.checkNickname({ nickname }),
+  });
+};
+
+export const useRegisterUserProfileMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UserProfile) => userApi.registerUserProfile(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useSubmitDstiMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: SubmitDstiRequest) => userApi.submitDsti(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateExperienceMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateExperienceRequest) => userApi.updateExperience(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateCareerMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateCareerRequest) => userApi.updateCareer(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateGithubMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateGithubRequest) => userApi.updateGithub(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateSelfIntroMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateSelfIntroRequest) => userApi.updateSelfIntro(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateTechSkillsMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateTechSkillsRequest) => userApi.updateTechSkills(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdatePositionsMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdatePositionsRequest) => userApi.updatePositions(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
+
+export const useUpdateNetworksMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data: UpdateNetworksRequest) => userApi.updateNetworks(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.USER.INFO() });
+    },
+  });
+};
