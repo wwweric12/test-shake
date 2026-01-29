0ee472647a56674fcba84e3b5171b34e05ee2744
김동영 <wwweric12@gmail.com>
2026-01-29 18:53:40 +0900

    fix: mock data response 변경

--- front/src/mocks/handlers/home.ts
+++ front/src/mocks/handlers/home.ts
@@ -6,16 +6,20 @@ import { HomeSummaryResponse } from '@/types/home';
 export const homeHandlers = [
   http.get(`${BASE_URL}/home/summary`, () => {
     const mockSummary: HomeSummaryResponse = {
-      nickname: '싸피테스트',
-      dsti: 'EDAU',
-      profileImageUrl: '',
-      totalUnreadMessages: 5,
-      totalLikeCount: 12,
-      others: {
-        profileImageUrl: ['https://picsum.photos/100', ''],
-        dsti: ['EDAU', 'PDAR'],
+      statusCode: 200,
+      message: 'OK',
+      data: {
+        nickname: '싸피테스트',
+        dsti: 'EDAU',
+        profileImageUrl: '',
+        totalUnreadMessages: 5,
+        totalLikeCount: 12,
+        others: {
+          profileImageUrl: ['https://picsum.photos/100', ''],
+          dsti: ['EDAU', 'PDAR'],
+        },
+        remainingSwipes: 10,
       },
-      remainingSwipes: 10,
     };
     return HttpResponse.json(mockSummary);
   }),
