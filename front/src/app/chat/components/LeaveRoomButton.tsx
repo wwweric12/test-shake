7707af3aea56945ded3cd70277d73ae3a8d5ccd9
DESKTOP-URKE2VL\SSAFY <zylee503@naver.com>
2026-01-31 11:24:05 +0900

    docs(chat): 채팅 관련 컴포넌트 및 훅 주석 정리

--- front/src/app/chat/components/LeaveRoomButton.tsx
+++ front/src/app/chat/components/LeaveRoomButton.tsx
@@ -1,29 +1,3 @@
-// 'use client';
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-
-// import QuitIcon from '@/assets/icon/door-open.svg';
-
-// import { LeaveRoomModal } from './LeaveRoomModal';
-
-// export function LeaveRoomButton() {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <>
-//       <button onClick={() => setOpen(true)} className="p-2">
-//         <Image src={QuitIcon} alt="방 나가기" width={20} height={20} />
-//       </button>
-
-//       {open && <LeaveRoomModal onClose={() => setOpen(false)} />}
-//     </>
-//   );
-// }
-/**
- * 채팅방 나가기 버튼 컴포넌트
- */
-
 'use client';
 
 import { useState } from 'react';
--- front/src/app/chat/components/ReportButton.tsx
+++ front/src/app/chat/components/ReportButton.tsx
@@ -1,29 +1,3 @@
-// 'use client';
-
-// import { useState } from 'react';
-// import Image from 'next/image';
-
-// import SirenIcon from '@/assets/icon/siren.svg';
-
-// import { ReportModal } from './ReportModal';
-
-// export function ReportButton() {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <>
-//       <button onClick={() => setOpen(true)} className="p-2">
-//         <Image src={SirenIcon} alt="신고하기" width={20} height={20} />
-//       </button>
-
-//       {open && <ReportModal onClose={() => setOpen(false)} />}
-//     </>
-//   );
-// }
-/**
- * 채팅방 신고 버튼 컴포넌트
- */
-
 'use client';
 
 import { useState } from 'react';
