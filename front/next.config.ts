c53ee375a55cc90912326f5e9f44e4a6b9d64080
김동영 <wwweric12@gmail.com>
2026-02-02 19:58:34 +0900

    feat: pwa 적용

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from 'next';
 
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public', // 서비스 워커 파일이 생성될 위치
+  cacheOnFrontEndNav: true, // 프론트엔드 네비게이션 시 캐싱 활성화
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true, // 온라인 상태가 되면 새로고침
+  disable: process.env.NODE_ENV === 'development', // 개발 환경에서는 PWA 비활성화
+  workboxOptions: {
+    disableDevLogs: true, // 개발 시 불필요한 로그 숨김
+  },
+});
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -11,4 +24,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
