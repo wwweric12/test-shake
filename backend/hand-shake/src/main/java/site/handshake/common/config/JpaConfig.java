f05df96e45bed37fe307480d8757596d8f45b13e
김동영 <wwweric12@gmail.com>
2026-02-03 13:36:55 +0900

    setting: initial commit

--- backend/hand-shake/src/main/java/site/handshake/common/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package site.handshake.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
