484870ab0f108fb715664644af9291f0916f9392
DESKTOP-URKE2VL\SSAFY <zylee503@naver.com>
2026-02-04 16:16:55 +0900

    refactor: auth 레이아웃을 라우트 그룹으로 분리
    - WebSocket 연결이 필요 없는 인증 페이지 분리
    - 불필요한 Provider 로딩 방지

--- front/src/app/(auth)/layout.tsx
+++ front/src/app/(auth)/layout.tsx
@@ -1,7 +1,3 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-custom-white relative flex h-dvh w-full max-w-[440px] min-w-[375px] flex-col overflow-hidden shadow-xl">
-      <main className="custom-scrollbar flex flex-1 flex-col overflow-y-auto">{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
